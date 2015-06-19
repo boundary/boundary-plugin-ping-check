3326e710753401cb68202bbe807898378fe42cb8
jdgwartney <davidg@boundary.com>
2015-06-19 16:51:03 -0700

    Missing local on the cmd variable caused the hostname from the poller to be added which causes an error the ping commnad

--- init.lua
+++ init.lua
@@ -14,23 +14,27 @@ local params = framework.params
 
 local HOST_IS_DOWN = -1
 
+-- Define a map of platform to ping commands
 local commands = {
   linux = { path = '/bin/ping', args = {'-n', '-w 2', '-c 1'} },
   win32 = { path = 'C:/windows/system32/ping.exe', args = {'-n', '1', '-w', '3000'} },
   darwin = { path = '/sbin/ping', args = {'-n', '-t 2', '-c 1'} }
 }
 
-local ping_command = commands[string.lower(os.type())] 
+-- Lookup the ping command to use based on the platform we are running on
+local ping_command = commands[string.lower(os.type())]
+
+-- If we do not support this platform then emit an error and exit
 if ping_command == nil then
   print("_bevent:"..(Plugin.name or params.name)..":"..(Plugin.version or params.version)..":Your platform is not supported.  We currently support Linux, Windows and OSX|t:error|tags:lua,plugin"..(Plugin.tags and framework.string.concat(Plugin.tags, ',') or params.tags))
   process:exit(-1)
 end
 
+-- Creates a poller for each item in our plugin parameter list in param.json
 local function createPollers (params, cmd) 
   local pollers = PollerCollection:new() 
   for _, item in ipairs(params.items) do
-    
-    cmd = clone(cmd)
+    local cmd = clone(cmd)
     table.insert(cmd.args, item.host)
     cmd.info = item.source
     cmd.callback_on_errors = true
